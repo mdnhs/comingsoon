46955658a00a49dce6101d270b0404cb7d85a6bb
nazmul.sourab <nazmul.sourab@technonext.com>
2025-04-09 16:05:47 +0600

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,8 +10,8 @@ const inter = Inter({
 })
 
 export const metadata = {
-  title: "Home - Comming soon Template",
-  description: 'This is a simple coming soon template built with NextJS and TailwindCSS. It is a lightweight and responsive template that can be used for various projects that require a "coming soon" page.',
+  title: "UpEasy - Experience the Next GENERATION of Subscriptions with us",
+  description: 'Experience the Next GENERATION of Subscriptions with us',
 }
 
 export default function RootLayout({
